622161290135d07f67ac7159d4e0b054d13bebd7
Muguai <fredrik-hammar@hotmail.com>
2023-08-01 16:10:25 +0200

    feat: added island generation

---
+++ IslandGenerator.cs
@@ -0,0 +1,169 @@
+public class IslandGenerator
+{
+    bool debug = false;
+    public int[,] InitIsland(int rows, int columns)
+    {
+        int iterations = 8; 
+        int minLandMass = 30;
+
+
+        int[,] island = GenerateIsland(rows, columns);
+
+        double desiredLandRatio = 0.3; 
+        int totalCells = rows * columns;
+        int desiredLandCells = (int)(totalCells * desiredLandRatio);
+
+        double initialChance = (desiredLandCells - 1) / (double)totalCells;
+
+
+        if (debug)
+        {
+            Console.WriteLine("--- ISLAND BEFORE ---");
+            PrintIsland(island);
+        }
+
+        island = CellularAutomaton(island, iterations, initialChance, minLandMass);
+
+        int landCells = island.Cast<int>().Count(cell => cell == 1);
+        Random random = new Random();
+        int startPositionIndex = random.Next(landCells);
+
+        if (!debug)
+        {
+            Console.WriteLine("--- ISLAND AFTER ---");
+            PrintIsland(island);    
+            Console.WriteLine("TOTAL LANDMASS --> " + landCells + " VS TOTAL CELLS " + totalCells);
+        }
+
+
+        int count = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (island[i, j] == 1)
+                {
+                    if (count == startPositionIndex)
+                    {
+                        island[i, j] = 2;
+                    }
+                    count++;
+                }
+            }
+        }
+        return island;
+    }
+
+    static int[,] GenerateIsland(int rows, int columns)
+    {
+        int[,] island = new int[rows, columns];
+        Random random = new Random();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                island[i, j] = 0;
+            }
+        }
+
+        int startRow = random.Next(rows / 4, rows / 2);
+        int startCol = random.Next(columns / 4, columns / 2);
+
+        island[startRow, startCol] = 1;
+
+        return island;
+    }
+
+    private int[,] CellularAutomaton(int[,] island, int iterations, double chance, int minLandMass)
+    {
+        int rows = island.GetLength(0);
+        int columns = island.GetLength(1);
+
+        Random random = new Random();
+
+        for (int iteration = 0; iteration < iterations; iteration++)
+        {
+            int[,] newIsland = new int[rows, columns];
+
+            for (int i = 1; i < rows - 1; i++)
+            {
+                for (int j = 1; j < columns - 1; j++)
+                {
+                    int neighborsCount = CountNeighbors(island, i, j);
+                    if (island[i, j] == 1 || (island[i, j] == 0 && neighborsCount > 0 && random.NextDouble() < chance))
+                    {
+                        newIsland[i, j] = 1;
+                    }
+                    if(debug)
+                        Console.WriteLine($"Cell [{i}, {j}] Neighbors: {neighborsCount}");
+                }
+            }
+
+            island = newIsland;
+
+            if(debug){
+                Console.WriteLine($"Island after iteration {iteration + 1}:");
+                PrintIsland(island);
+            }
+            int currentLandMass = (island.Cast<int>().Count(cell => cell == 1));
+            if(iteration + 1 == iterations && currentLandMass < minLandMass){
+                Console.WriteLine("Correcty " + currentLandMass);
+                iteration--;
+            }
+        }
+
+        return island;
+    }
+
+    private int CountNeighbors(int[,] island, int row, int col)
+    {
+        int count = 0;
+
+        int rows = island.GetLength(0);
+        int columns = island.GetLength(1);
+
+        int[] rowOffsets = { -1, 1, 0, 0 };
+        int[] colOffsets = { 0, 0, -1, 1 };
+
+        for (int direction = 0; direction < 4; direction++)
+        {
+            int newRow = row + rowOffsets[direction];
+            int newCol = col + colOffsets[direction];
+
+            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < columns)
+            {
+                if (island[newRow, newCol] == 1)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void PrintIsland(int[,] island)
+    {
+        int rows = island.GetLength(0);
+        int columns = island.GetLength(1);
+
+        Console.WriteLine("Island:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (island[i, j] == 0)
+                {
+                    Console.Write(".");
+                }
+                else if (island[i, j] == 1)
+                {
+                    Console.Write("X");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+using System;
+
+public static class Program
+{
+
+    public static void Main()
+    {
+        Setup();
+    }
+
+    private static void Setup()
+    {
+
+        AnsiConsole.Write(
+            new FigletText("Dumb Adventure")
+                .LeftJustified()
+                .Color(Color.Red));
+
+        var heroClass = AnsiConsole.Prompt(
+          new SelectionPrompt<string>()
+          .Title("Choose your [green]Class[/]?")
+          .PageSize(5)
+          .AddChoices(new[] {
+                "Barbarian", "Wizard", "Archer",
+                "SwashBuckler",
+      }));
+        string dialog = "";
+        switch (heroClass)
+        {
+            case "Barbarian":
+                dialog = "Barbarian sucks... but oh well its your choice i guess";
+                break;
+            case "Wizard":
+                dialog = "Ok Wizard nice. You should name yourself Rincewind. Trust me its a good name";
+                break;
+            case "Archer":
+                dialog = "You made the correct choice well done, this is the coolest class";
+                break;
+            case "SwashBuckler":
+                dialog = "Sorry the rogue is on a lunchbreak so this is all you get";
+                break;
+        }
+
+        AnsiConsole.WriteLine(dialog);
+
+        var name = AnsiConsole.Ask<string>("What's your heros [green]name[/]?");
+
+        AnsiConsole.MarkupLine("You dumb name is: [yellow]{0}[/]", name);
+
+        Console.Clear();
+
+        AnsiConsole.Clear();
+
+        IslandGenerator islandGenerator = new IslandGenerator();
+        int rows = 10;
+        int columns = 10;
+        int[,] island = islandGenerator.InitIsland(rows, columns);
+
+
+        var canvas = new Canvas(columns, rows);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int value = island[i, j];
+                if (value == 0)
+                {
+                    canvas.SetPixel(j, i, Color.Blue);
+                }
+                else if (value == 1)
+                {
+                    canvas.SetPixel(j, i, Color.Green);
+                }else if (value == 2)
+                {
+                    canvas.SetPixel(j, i, Color.Yellow);
+                }
+            }
+        }
+
+        // Render the canvas
+        AnsiConsole.Write(canvas);
+
+        var direction = AnsiConsole.Ask<string>($"Where you wanna go [green]{name}[/]?");
+        AnsiConsole.Clear();
+
+    }
+
+    
+
+    
+}
